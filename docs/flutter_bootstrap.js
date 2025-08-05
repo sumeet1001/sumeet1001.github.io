e96624b4b14a02401d52db1944f7fabd77db6e40
Sumeet Sharma <sssumeet13@gmail.com>
2025-08-05 15:51:33 +0530

    updated code

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2003593709"
+    serviceWorkerVersion: "413369567"
   }
 });
